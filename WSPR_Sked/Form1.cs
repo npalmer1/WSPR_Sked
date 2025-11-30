20b41da2c5ec922d3bf143c658fa4a72bc8b926b
npalmer1 <ntpalmer1@yahoo.co.uk>
2025-11-30 14:19:40 +0000

    WS0115

--- WSPR_Sked/Form1.cs
+++ WSPR_Sked/Form1.cs
@@ -295,7 +295,7 @@ namespace WSPR_Sked
         {
 
             System.Version version = Assembly.GetExecutingAssembly().GetName().Version;
-            string ver = "0.1.14";
+            string ver = "0.1.15";
             this.Text = "WSPR Scheduler                       V." + ver + "    GNU GPLv3 License";
             dateformat = "yyyy-MM-dd";
             OpSystem = 0; //default to Windows
