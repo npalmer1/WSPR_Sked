cde75ca9ef523d6125c73e468b585d8c2a005c59
npalmer1 <ntpalmer1@yahoo.co.uk>
2026-04-07 13:58:24 +0100

    WS0138b

--- WSPR_Sked/Program.cs
+++ WSPR_Sked/Program.cs
@@ -19,29 +19,12 @@ namespace WSPR_Sked
             Application.SetCompatibleTextRenderingDefault(false);
             try
             {
-                string logPath = @"C:\temp\crash_log.txt";
 
-                // make sure the folder exists
-                Directory.CreateDirectory(@"C:\temp");
-
-                Application.ThreadException += (sender, e) =>
-                {
-                    File.WriteAllText(logPath, e.Exception.ToString());
-                    MessageBox.Show("Error: " + e.Exception.Message + "\n\nDetails saved to " + logPath);
-                };
-
-                AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
-                {
-                    File.WriteAllText(logPath, e.ExceptionObject.ToString());
-                };
                 Application.Run(new Form1());
-                File.AppendAllText(@"C:\temp\crash_log.txt", "App exited cleanly" + Environment.NewLine);
+
             }
-            catch (Exception ex)
+            catch
             {
-                Directory.CreateDirectory(@"C:\temp");
-                File.WriteAllText(@"C:\temp\crash_log.txt", ex.ToString());
-                MessageBox.Show("Fatal error: " + ex.Message);
             }
           
          
