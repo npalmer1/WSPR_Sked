bb9451ecab871d5e61e87f120d441e74545655c5
npalmer1 <ntpalmer1@yahoo.co.uk>
2026-06-07 11:04:24 +0100

    WS0144

--- WSPR_Sked/Form1.cs
+++ WSPR_Sked/Form1.cs
@@ -6051,7 +6051,7 @@ namespace WSPR_Sked
                 //noSkedcheckBox.Checked = false;
             }*/
             bool nearTrigger = (m % 2 == 1 && s >= 49) || (m % 2 == 0 && s <= 4);
-            if (keypresses > 120 && (s == 6) && !slotgroupBox.Visible && !nearTrigger)
+            if (keypresses > 90 && (s == 6) && !slotgroupBox.Visible && !nearTrigger)
             {
                 currHour(true, trackSlotscheckBox.Checked);
                 if (!trackSlotscheckBox.Checked) { keypresses = 0; }
@@ -7368,7 +7368,7 @@ namespace WSPR_Sked
              }*/
 
             bool nearTrigger = (m % 2 == 1 && s >= 49) || (m % 2 == 0 && s <= 4);
-            if (keypresses > 120 && s == 6 && !slotgroupBox.Visible && !nearTrigger)
+            if (keypresses > 90 && s == 6 && !slotgroupBox.Visible && !nearTrigger)
             {
                 currHour(true, trackSlotscheckBox.Checked);
                 if (!trackSlotscheckBox.Checked) { keypresses = 0; }
